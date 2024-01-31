3b836af59897b1fe14df42497142129c6153dea9
pranav manikandan <112387943+pranavm181@users.noreply.github.com>
2024-01-31 20:39:14 +0530

    connected database

---
+++ lib/service/database.dart
@@ -0,0 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseMethods {
+  Future addWorkerDetails(Map<String, dynamic> workerInfoApp, String id) async {
+    return await FirebaseFirestore.instance
+        .collection("Worker")
+        .doc(id)
+        .set(workerInfoApp);
+  }
+}
